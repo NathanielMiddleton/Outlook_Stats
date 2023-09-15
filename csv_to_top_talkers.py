42fb58e392f8bc64a6df8e1d336a04919158c703
Nathaniel Middleton <nathanielm@alleninstitute.org>
2023-09-15 08:43:15 -0700

    top talkers

---
+++ csv_to_top_talkers.py
@@ -0,0 +1,22 @@
+import csv
+import pandas as pd
+pd.set_option('display.max_rows', 50, 'display.min_rows', 50)
+
+filename = 'filename.csv'
+
+with open(filename, 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    headers = next(reader)
+    print(f'The available fields in {filename} are: {headers}')
+
+df = pd.read_csv(filename)
+
+# Trends in from addresses
+from_trends = df['From: (Name)'].value_counts(normalize=True) * 100
+print('Trends in from addresses: (percentage of total):')
+print(from_trends, "%")
+
+# Trends in email subject
+subject_trends = df['Subject'].value_counts(normalize=True) * 100
+print('Trends in email Subject:')
+print(subject_trends)
